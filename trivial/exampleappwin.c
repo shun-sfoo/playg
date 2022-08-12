ef4930a2673c2bf154aa796a37beee54246f1e68
shun-sfoo <ganymede0915@gmail.com>
2022-08-12 17:16:04 +0800

    🎸 feat: trivial app 05: search bar

--- trivial/exampleappwin.c
+++ trivial/exampleappwin.c
@@ -10,9 +10,47 @@ struct _ExampleAppWindow
   GSettings *settings;
   GtkWidget *stack;
   GtkWidget *gears;
+  GtkWidget *search;
+  GtkWidget *searchbar;
 };
 
 G_DEFINE_TYPE (ExampleAppWindow, example_app_window, GTK_TYPE_APPLICATION_WINDOW)
+
+static void
+search_text_changed (GtkEntry *entry, ExampleAppWindow *win)
+{
+  const char *text;
+  GtkWidget *tab;
+  GtkWidget *view;
+  GtkTextBuffer *buffer;
+  GtkTextIter start, match_start, match_end;
+
+  text = gtk_editable_get_text (GTK_EDITABLE (entry));
+
+  if (text[0] == '\0')
+    return;
+
+  tab = gtk_stack_get_visible_child (GTK_STACK (win->stack));
+  view = gtk_scrolled_window_get_child (GTK_SCROLLED_WINDOW (tab));
+  buffer = gtk_text_view_get_buffer (GTK_TEXT_VIEW (view));
+
+  /* Very simple-minded search implementation */
+  gtk_text_buffer_get_start_iter (buffer, &start);
+  if (gtk_text_iter_forward_search (&start, text, GTK_TEXT_SEARCH_CASE_INSENSITIVE, &match_start, &match_end, NULL))
+    {
+      gtk_text_buffer_select_range (buffer, &match_start, &match_end);
+      gtk_text_view_scroll_to_iter (GTK_TEXT_VIEW (view), &match_start, 0.0, FALSE, 0.0, 0.0);
+    }
+}
+
+static void
+visible_child_changed (GObject *stack, GParamSpec *pspec, ExampleAppWindow *win)
+{
+  if (gtk_widget_in_destruction (GTK_WIDGET (stack)))
+    return;
+
+  gtk_search_bar_set_search_mode (GTK_SEARCH_BAR (win->searchbar), FALSE);
+}
 
 static void
 example_app_window_init (ExampleAppWindow *win)
@@ -30,6 +68,8 @@ example_app_window_init (ExampleAppWindow *win)
   win->settings = g_settings_new ("org.gtk.exampleapp");
 
   g_settings_bind (win->settings, "transition", win->stack, "transition-type", G_SETTINGS_BIND_DEFAULT);
+
+  g_object_bind_property (win->search, "active", win->searchbar, "search-mode-enabled", G_BINDING_BIDIRECTIONAL);
 }
 
 static void
@@ -50,8 +90,14 @@ example_app_window_class_init (ExampleAppWindowClass *class)
   G_OBJECT_CLASS (class)->dispose = example_app_window_dispose;
 
   gtk_widget_class_set_template_from_resource (GTK_WIDGET_CLASS (class), "/org/gtk/exampleapp/window.glade");
+
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, stack);
   gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, gears);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, search);
+  gtk_widget_class_bind_template_child (GTK_WIDGET_CLASS (class), ExampleAppWindow, searchbar);
+
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), search_text_changed);
+  gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), visible_child_changed);
 }
 
 ExampleAppWindow *
@@ -98,4 +144,6 @@ example_app_window_open (ExampleAppWindow *win, GFile *file)
   gtk_text_buffer_apply_tag (buffer, tag, &start_iter, &end_iter);
 
   g_free (basename);
+
+  gtk_widget_set_sensitive (win->search, TRUE);
 }
